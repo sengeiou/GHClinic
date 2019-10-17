89caf0588f9f39861524364da383382823988721
qwe <asda>
2019-10-17 15:24:58 +0800

    qweqwe

--- h5/src/app/order/order.page.ts
+++ h5/src/app/order/order.page.ts
@@ -7,12 +7,13 @@ import { AppUtil } from '../app.util';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MemberApi } from 'src/providers/member.api';
 import { DindanApi } from 'src/providers/dindan.api';
-
+import { WechatApi } from 'src/providers/wechat.api';
+declare let WeixinJSBridge: any;
 @Component({
   selector: 'app-order',
   templateUrl: 'order.page.html',
   styleUrls: ['order.page.scss'],
-  providers: [DindanApi]
+  providers: [DindanApi,WechatApi]
 })
 export class OrderPage extends AppBase {
 
@@ -23,6 +24,7 @@ export class OrderPage extends AppBase {
     public alertCtrl: AlertController,
     public activeRoute: ActivatedRoute,
     public dindanApi: DindanApi,
+    public wechatApi:WechatApi,
     public sanitizer: DomSanitizer,
     public memberApi: MemberApi) {
     super(router, navCtrl, modalCtrl, toastCtrl, alertCtrl, activeRoute);
@@ -74,17 +76,45 @@ export class OrderPage extends AppBase {
 
   }
   successfulTrade() {
-     
+     var wechatapi=this.wechatApi;
     var api=this.dindanApi;
     
       api.createorder({}).then((res)=>{
+            
+        wechatapi.prepay({ id: res.id, h5: "Y" }).then((payret) => {
+          if (payret.code != 0) {
+
+            this.showAlert(payret.result);
+            return;
+          }
+
+          console.log(payret);
+          //wx.requestPayment(payret)
+
+          WeixinJSBridge.invoke(
+            'getBrandWCPayRequest', payret,
+            (res) => {
+              if(res.err_msg == "get_brand_wcpay_request:ok" ){
+
+
+              
+                this.navigate("successful-trade");
+              
+
+
+              } else {
+                this.showAlert(res.errMsg);
+              }
+            });
+        });
 
        console.log(res);
 
       })
+      
    
    return
-    this.navigate("successful-trade")
+ 
   }
 
 }
--- h5/src/providers/wechat.api.ts
+++ h5/src/providers/wechat.api.ts
@@ -43,4 +43,72 @@ export class WechatApi {
             });
     }
 
+
+    public notify4(data, showLoadingModal: boolean = true) {
+        var url = ApiConfig.getApiUrl() + 'wechat/notify4';
+        var headers = ApiConfig.GetHeader(url, data);
+        let options = new RequestOptions({ headers: headers });
+        let body = ApiConfig.ParamUrlencoded(data);
+        let loading = null;
+
+        if (showLoadingModal) {
+            loading = ApiConfig.GetLoadingModal();
+        }
+
+        return this.http.post(url, body, options).toPromise()
+            .then((res) => {
+                if (ApiConfig.DataLoadedHandle('wechat/notify4', data, res)) {
+                    if (showLoadingModal) {
+                        ApiConfig.DimissLoadingModal();
+                    }
+                    if (res==null) {
+                        return null;
+                    }
+                    return res.json();
+                } else {
+                    return Promise.reject(res);
+                }
+            })
+            .catch(err => {
+                if (showLoadingModal) {
+                    ApiConfig.DimissLoadingModal();
+                }
+                return ApiConfig.ErrorHandle('wechat/notify4', data, err);
+            });
+    }
+
+
+    public prepay(data, showLoadingModal: boolean = true) {
+        var url = ApiConfig.getApiUrl() + 'wechat/prepay';
+        var headers = ApiConfig.GetHeader(url, data);
+        let options = new RequestOptions({ headers: headers });
+        let body = ApiConfig.ParamUrlencoded(data);
+        let loading = null;
+
+        if (showLoadingModal) {
+            loading = ApiConfig.GetLoadingModal();
+        }
+
+        return this.http.post(url, body, options).toPromise()
+            .then((res) => {
+                if (ApiConfig.DataLoadedHandle('wechat/prepay', data, res)) {
+                    if (showLoadingModal) {
+                        ApiConfig.DimissLoadingModal();
+                    }
+                    if (res==null) {
+                        return null;
+                    }
+                    return res.json();
+                } else {
+                    return Promise.reject(res);
+                }
+            })
+            .catch(err => {
+                if (showLoadingModal) {
+                    ApiConfig.DimissLoadingModal();
+                }
+                return ApiConfig.ErrorHandle('wechat/prepay', data, err);
+            });
+    }
+
 }
