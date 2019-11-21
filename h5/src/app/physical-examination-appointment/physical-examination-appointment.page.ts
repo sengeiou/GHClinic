b74e4219b49080a95c9bf78a4eec0bb28ef2f5f7
ss <asda>
2019-11-21 20:07:06 +0800

    qwe

--- h5/src/app/physical-examination-appointment/physical-examination-appointment.page.ts
+++ h5/src/app/physical-examination-appointment/physical-examination-appointment.page.ts
@@ -125,7 +125,7 @@ export class PhysicalExaminationAppointmentPage extends AppBase {
       if (ret) {
         var api=this.tijianApi;
         api.create({setmeal_id:this.params.id,hospital_id:this.params.yiyuanid, patientname:canshu.name,patientmobile:canshu.shouji,
-          patientid:canshu.id, openid:this.openid,beizhu:canshu.beizhu
+          patientid:canshu.id, openid:this.openid,beizhu:canshu.beizhu,ordertime:this.params.riqi
         }).then((res)=>{
          
           if(res.code=="233"){
